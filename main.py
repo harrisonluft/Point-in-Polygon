a4c733f01419e0fe0df852db05ef7e3cb0141769
Harrison Luft <harrisonluft@gmail.com>
2020-11-09 11:39:25 +0000

    feat: loop to find intersection for each ray and polygon boundary, checks for point on boundary and overlapping lines.

--- main.py
+++ main.py
@@ -41,37 +41,59 @@ def on_line_seg(x1, y1, x2, y2, input_x, input_y): # using point/slope formula t
         # using mbr method to confirm point is in between points of line segments
         on_border = on_line and line_seg_mbr
         if on_border:
-            return 'Boundary'
+            return True
         else:
-            return 'Unclassified'
+            return False
     else:
         on_border = (x2 == input_x) and (min(y1, y2) <= input_y <= max(y1, y2))
         if on_border:
-            return 'Boundary'
+            return True
         else:
-            return 'Unclassified'
+            return False
 
 # adapted from https://silentmatt.com/rectangle-intersection/
-def mbr_seg(x1, y1, x2, y2, x3, y3, x4, y4): # bounding boxes of line segments must overlap for there to be intersection
+# Mbr's must intersect for lines to intersect.
+# Because the ray's extend beyond the polygon's maximum bounds, if the boxes intersect
+# there is an intersection.
+
+# This does not take into account overlapping segments, will build another function for that.
+# First check:
+def mbr_seg(x1, y1, x2, y2, x3, y3, x4, y4):
     mbr_overlap = x1 <= x4 and x2 >= x3 and y1 <= y4 and y2 >= y3
-    return mbr_overlap # True/False for intersection of boxes
+    return mbr_overlap # True/False for intersection of bounding boxes
+
+def overlap_check(x1, y1, x2, y2, x3, y3, x4, y4):
+    y_overlap = y1 == y2 and y1 == y3 and y1 == y4
+    x_overlap = x1 <= x4 and x2 >= x3
+    overlap = y_overlap and x_overlap
+    return overlap
 
 # Adapted from https://rosettacode.org/wiki/Find_the_intersection_of_two_lines
-def line_intersect(Ax1, Ay1, Ax2, Ay2, Bx1, By1, Bx2, By2): # only identifies non collinear intersections, returns None for collinear lines
+def line_intersect(x1, y1, x2, y2, x3, y3, x4, y4): # only identifies non collinear intersections, returns None for collinear lines
 # returns a (x, y) tuple or None if there is no intersection
 # will use the (x, y) return to adjust for crossing vertices
-    d = (By2 - By1) * (Ax2 - Ax1) - (Bx2 - Bx1) * (Ay2 - Ay1)
+    d = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
     if d:
-        uA = ((Bx2 - Bx1) * (Ay1 - By1) - (By2 - By1) * (Ax1 - Bx1)) / d
-        uB = ((Ax2 - Ax1) * (Ay1 - By1) - (Ay2 - Ay1) * (Ax1 - Bx1)) / d
+        uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / d
+        uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / d
     else:
         return
     if not (0 <= uA <= 1 and 0 <= uB <= 1):
         return
-    x = Ax1 + uA * (Ax2 - Ax1)
-    y = Ay1 + uA * (Ay2 - Ay1)
-
+    x = x1 + uA * (x2 - x1)
+    y = y1 + uA * (y2 - y1)
     return x, y
+
+
+def rca(x1, y1, x2, y2, x3, y3, x4, y4):
+    # if mbr intersect, check collinearity as a marker for checking vertices
+    if overlap_check(x1, y1, x2, y2, x3, y3, x4, y4):
+        return 'Collinear'
+    else:
+        return line_intersect(x1, y1, x2, y2, x3, y3, x4, y4)
+
+    # if mbr_seg(x1, y1, x2, y2, x3, y3, x4, y4):
+
 
 
  # Polygon Class and creating MBR
@@ -103,20 +125,6 @@ class Poly:
         self.lines.append(tuple([p1,(self.x_values[0],self.y_values[0])]))
         print(self.lines)
 
-    def classify(self, ray_lines):
-        res = []
-        for item in ray_lines:
-            temp = []
-            if item in self.poly_points:
-                temp.append('Boundary')
-            else:
-                for line in self.lines:
-                    temp.append(on_line_seg(line[0][0], line[0][1], line[1][0], line[1][1], item[0][0], item[0][1]))
-            res.append(temp)
-        self.results = res
-        print(self.results)
-
-
     def mbr(self):
         self.min_x = minimum(self.x_values)
         self.min_y = minimum(self.y_values)
@@ -130,10 +138,31 @@ class Poly:
 
     def classify_mbr(self, x, y):
         if (x <= self.max_x) and (y <= self.max_y) and (x >= self.min_x) and (y >= self.min_y):
-            return 'inside'
+            return 'Inside'
         else:
-            return 'outside'
+            return 'Outside'
 
+
+    def classify(self, ray_lines):
+        res = []
+        for item in ray_lines:
+            temp = []
+            if item in self.poly_points:
+                temp.append('Boundary')
+            elif self.classify_mbr(item[0][0], item[0][1]) == 'Outside':
+                temp.append('Outside')
+            else:
+                for line in self.lines:
+                    if on_line_seg(line[0][0], line[0][1], line[1][0], line[1][1], item[0][0], item[0][1]):
+                        temp.append('Boundary')
+                    else:
+                        temp.append(rca(line[0][0], line[0][1], line[1][0], line[1][1], item[0][0], item[0][1], item[1][0], item[1][1]))
+
+            res.append(temp)
+        self.results = res
+        print(self.results)
+    # def classify_intersect(self):
+    #     for
 
 # Creating input point class
 class Point:
@@ -141,7 +170,7 @@ class Point:
         self.points = points
 
     def get_point(self, i):
-        return self.points[i][1],self.points[i][2]
+        return self.points[i][0],self.points[i][1]
 
     def ray_lines(self, mbr_max_x):
         self.rca_x = mbr_max_x + 1
@@ -150,41 +179,28 @@ class Point:
             self.ray_lines.append(tuple([(self.points[i][0], self.points[i][1]), (self.rca_x, self.points[i][1])]))
         return self.ray_lines
 
-    # def ray_lines(self):
-    #     self.ray_lines = []
-    #     for i in self.points:
-    #         self.ray_lines.append([self.points[i],self.ray_points[i]])
-    #     return self.ray_lines
 
-    # def get_all_points(self):
-    #     points_list = []
-    #     for i in range(len(self.points)):  # converting coordinates to float
-    #         points_list.append([float(self.points[i][1]), float(self.points[i][2])])
-    #     return points_list
 
-    # def get_id(self):
-    #     return self.id
-    # def get_x(self):
-    #     return self.x
-    #
-    # def get_y(self):
-    #     return self.y
 def main():
     poly = import_data("C:/Users/17075/Assignment_1/Project Template/polygon.csv")
     print(poly)
     points = import_data("C:/Users/17075/Assignment_1/Project Template/input.csv")
     print(points)
-    test = Poly(poly)
-    test.mbr()
+    test = Poly(poly) # init poly class, create x and y lists
+    test.mbr() # create bounding box for polygon
 
 
-    point_test = Point(points)
-    print(point_test.ray_lines(test.max_x))
+    point_test = Point(points) # init point class
+    #print(rca(3.0, 5.0, 2.0, 6.0, 1.0, 5.5, 5.0, 5.5))
+    #print(rca(2.0, 6.0, 1.0, 5.0, 1.0, 5.5, 5.0, 5.5))
+    # print(mbr_seg(2.0, 6.0, 1.0, 5.0, 1.0, 5.5, 5.0, 5.5))
 
-    test.classify(point_test.ray_lines)
+    point_test.ray_lines(test.max_x) # create ray's for each point based on poly bounding box
 
-    # point_test.get_point(0)
-    # x,y = point_test.get_point(0)
+    test.classify(point_test.ray_lines) # classify points
+
+    # point_test.get_point(1)
+    # x, y = point_test.get_point(1)
     # print('({},{})'.format(x,y))
     #print(f'({x},{y})')
     #print(test.classify_mbr(x,y))
